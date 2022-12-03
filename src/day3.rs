aea6763812c7dc511f24f232af39966a65a66360
Owen O'Malley <omalley@apache.org>
2022-12-02 18:36:26 -0800

    Switched to an array of bool.

--- src/day3.rs
+++ src/day3.rs
@@ -1,88 +1,127 @@
-use std::collections::HashSet;
-
+/// Define a set of items using an array of bool
 #[derive(Debug)]
-pub struct Compartment {
-  items: HashSet<char>,
+pub struct Contents {
+  items: [bool; Contents::SIZE],
 }
 
-impl Compartment {
+impl Contents {
+  const SIZE: usize = 56;
+
+  /// Parse from a list o characters
   fn from_chars(chars: &[char]) -> Self {
-    let mut items = HashSet::new();
+    let mut items = [false; Self::SIZE];
     for ch in chars {
-      items.insert(*ch);
+      items[Self::position(*ch)] = true;
     }
-    Compartment{items}
+    Contents{items}
   }
-}
 
-#[derive(Debug)]
-pub struct Rucksack {
-  parts: Vec<Compartment>,
+  /// Map the characters to position in the array
+  fn position(ch: char) -> usize {
+    match ch {
+      'a'..='z' => (ch as usize) - ('a' as usize),
+      'A'..='Z' => (ch as usize) - ('A' as usize) + 26,
+      _ => panic!("Bad value {ch}")
+    }
+  }
+
+  /// Map the position back into a character
+  fn item(pos: usize) -> char {
+    match pos {
+      0..=25 => char::from_u32(('a' as u32) + pos as u32).unwrap(),
+      26..=51 => char::from_u32(('A' as u32) + pos as u32 - 26).unwrap(),
+      _ => panic!("Bad position {pos}")
+    }
+  }
+
+  /// Create the intersection of a list of sets
+  fn intersect(sets: &[Self]) -> Self {
+    let mut items = [true; Self::SIZE];
+    for s in sets {
+      for i in 0..Self::SIZE {
+        if !s.items[i] {
+          items[i] = false;
+        }
+      }
+    }
+    Contents{items}
+  }
+
+  /// Create the union oof a list of sets
+  fn union(sets: &[Self]) -> Self {
+    let mut items = [false; Self::SIZE];
+    for s in sets {
+      for i in 0..Self::SIZE {
+        if s.items[i] {
+          items[i] = true;
+        }
+      }
+    }
+    Contents{items}
+  }
+
+  /// Return the first item in the set
+  fn first_item(&self) -> Option<char> {
+    for i in 0..Self::SIZE {
+      if self.items[i] {
+        return Some(Self::item(i))
+      }
+    }
+    None
+  }
 }
 
 fn priority(ch: char) -> i32 {
-  match ch {
-    'a'..='z' => (ch as i32) - ('a' as i32) + 1,
-    'A'..='Z' => (ch as i32) - ('A' as i32) + 27,
-    _ => panic!("Bad value {ch}")
-  }
+  Contents::position(ch) as i32 + 1
+}
+
+/// Define Rucksacks as lists of sets
+#[derive(Debug)]
+pub struct Rucksack {
+  parts: Vec<Contents>,
 }
 
 impl Rucksack {
+
+  /// Parse a rucksack from a string.
+  /// Divides the string in half to get two compartments.
   fn from_str(s: &str) -> Self {
     let chars: Vec<char> = s.chars().collect();
     assert_eq!(chars.len() % 2, 0);
     let parts = chars.chunks(chars.len() / 2)
-      .map(|ch| Compartment::from_chars(ch)).collect();
+      .map(|ch| Contents::from_chars(ch)).collect();
     Rucksack{parts}
   }
 
-  fn find_match(&self) -> i32 {
-    for ch in self.parts[0].items.intersection(&self.parts[1].items) {
-      return priority(*ch);
-    }
-    0
+  /// Find the common item between the compartments
+  fn find_match(&self) -> Option<char> {
+    Contents::intersect(&self.parts).first_item()
   }
 
-  fn contains(&self, ch: char) -> bool {
-    for comp in &self.parts {
-      if comp.items.contains(&ch) {
-        return true
-      }
-    }
-    false
-  }
-
-  fn items(&self) -> Vec<char> {
-    self.parts.iter()
-      .flat_map(|c| c.items.iter().map(|c| *c).collect::<Vec<char>>())
-      .collect()
+  /// Get the set of all items in the rucksack.
+  fn items(&self) -> Contents {
+    Contents::union(&self.parts)
   }
 }
 
-fn find_match(group: &[Rucksack]) -> i32 {
-  assert_ne!(group.len(), 0);
-  'item: for ch in &group[0].items() {
-    for other in &group[1..] {
-      if !other.contains(*ch) {
-        continue 'item
-      }
-    }
-    return priority(*ch)
-  }
-  0
+/// Find the common item in a group of rucksacks
+fn find_match(group: &[Rucksack]) -> Option<char> {
+  let contents: Vec<Contents> = group.iter().map(|r| r.items()).collect();
+  Contents::intersect(&contents).first_item()
 }
 
 pub fn generator(input: &str) -> Vec<Rucksack> {
   input.lines().map(|l| Rucksack::from_str(l)).collect()
 }
 
+/// Find the common item in each sack and sum the priorities.
 pub fn part1(input: &[Rucksack]) -> i32 {
-  input.iter().map(|r| r.find_match()).sum()
+  input.iter().map(|r| priority(r.find_match().unwrap())).sum()
 }
 
+/// Group the sacks into sets of 3, find the common item, and sum the priorities.
 pub fn part2(input: &[Rucksack]) -> i32 {
-  input.chunks(3).map(|group| find_match(group)).sum()
+  input.chunks(3).map(|group| priority(find_match(group).unwrap())).sum()
 }
 
 #[cfg(test)]
@@ -95,12 +134,6 @@ mod tests {
                        wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n\
                        ttgJtRGJQctTZtZT\n\
                        CrZsJsPPZsGzwwsLwLmpwMDw";
-
-  fn parsing_test() {
-    for x in generator(INPUT) {
-      println!("{:?}", x);
-    }
-  }
 
   #[test]
   fn test_part1() {
