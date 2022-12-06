5cd0be2f1822ab661f3fcb6d91490f85f7b91d46
Owen O'Malley <oomalley@linkedin.com>
2022-12-06 09:42:24 -0800

    Move InputType from str to Vec<char>.

--- src/day6.rs
+++ src/day6.rs
@@ -1,8 +1,8 @@
-type InputType = str;
+type InputType = Vec<char>;
 type OutputType = usize;
 
-pub fn generator(input: &str) -> &InputType {
-  input
+pub fn generator(input: &str) -> InputType {
+  input.chars().collect()
 }
 
 fn is_all_unique(w: &[(usize, char)]) -> bool {
@@ -16,8 +16,8 @@ fn is_all_unique(w: &[(usize, char)]) -> bool {
   true
 }
 
-fn find_unique(input: &str, size: usize) -> usize {
-  let chars: Vec<(usize, char)> = input.chars().enumerate().collect();
+fn find_unique(input: &Vec<char>, size: usize) -> usize {
+  let chars: Vec<(usize, char)> = input.iter().enumerate().map(|(i,&c)| (i,c)).collect();
   chars.windows(size).find(| &w | is_all_unique(w)).unwrap().first().unwrap().0 + size
 }
 
@@ -31,23 +31,23 @@ pub fn part2(input: &InputType) -> OutputType {
 
 #[cfg(test)]
 mod tests {
-  use crate::day6::{part1, part2};
+  use crate::day6::{generator, part1, part2};
 
   #[test]
   fn test_part1() {
-    assert_eq!(7, part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
-    assert_eq!(5, part1("bvwbjplbgvbhsrlpgdmjqwftvncz"));
-    assert_eq!(6, part1("nppdvjthqldpwncqszvftbrmjlhg"));
-    assert_eq!(10, part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
-    assert_eq!(11, part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+    assert_eq!(7, part1(&generator("mjqjpqmgbljsphdztnvjfqwrcgsmlb")));
+    assert_eq!(5, part1(&generator("bvwbjplbgvbhsrlpgdmjqwftvncz")));
+    assert_eq!(6, part1(&generator("nppdvjthqldpwncqszvftbrmjlhg")));
+    assert_eq!(10, part1(&generator("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")));
+    assert_eq!(11, part1(&generator("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")));
   }
 
   #[test]
   fn test_part2() {
-    assert_eq!(19, part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"));
-    assert_eq!(23, part2("bvwbjplbgvbhsrlpgdmjqwftvncz"));
-    assert_eq!(23, part2("nppdvjthqldpwncqszvftbrmjlhg"));
-    assert_eq!(29, part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"));
-    assert_eq!(26, part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"));
+    assert_eq!(19, part2(&generator("mjqjpqmgbljsphdztnvjfqwrcgsmlb")));
+    assert_eq!(23, part2(&generator("bvwbjplbgvbhsrlpgdmjqwftvncz")));
+    assert_eq!(23, part2(&generator("nppdvjthqldpwncqszvftbrmjlhg")));
+    assert_eq!(29, part2(&generator("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")));
+    assert_eq!(26, part2(&generator("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")));
   }
 }
