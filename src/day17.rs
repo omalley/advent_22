15397e385c2c7a9ba9d6e3556854f167a6421aad
Owen O'Malley <omalley@apache.org>
2022-12-17 20:18:36 -0800

    day 17 part 2

--- src/day17.rs
+++ src/day17.rs
@@ -1,9 +1,9 @@
-use std::fmt::{Display, Formatter};
+use std::collections::HashSet;
 
 type InputType = Vec<Wind>;
 type OutputType = usize;
 
-#[derive(Clone,Debug,Eq,PartialEq)]
+#[derive(Clone,Copy,Debug,Eq,PartialEq)]
 pub enum Wind {
   Left,
   Right,
@@ -32,11 +32,11 @@ pub fn generator(input: &str) -> InputType {
     .map(| c | Wind::parse(c)).collect()
 }
 
-#[derive(Clone,Debug,Eq,PartialEq)]
+#[derive(Clone,Copy,Debug,Eq,PartialEq)]
 enum PieceKind {
   Bar,
   Plus,
-  El,
+  L,
   I,
   Square,
 }
@@ -47,7 +47,7 @@ impl PieceKind {
     match self {
       PieceKind::Bar => &[0xf],
       PieceKind::Plus => &[0x2, 0x7, 0x2],
-      PieceKind::El => &[0x7, 0x4, 0x4],
+      PieceKind::L => &[0x7, 0x4, 0x4],
       PieceKind::I => &[0x1; 4],
       PieceKind::Square => &[0x3; 2],
     }
@@ -56,7 +56,7 @@ impl PieceKind {
   fn size(&self) -> (usize,usize) {
     match self {
       PieceKind::Bar => (4, 1),
-      PieceKind::Plus | PieceKind::El => (3, 3),
+      PieceKind::Plus | PieceKind::L => (3, 3),
       PieceKind::I => (1, 4),
       PieceKind::Square => (2, 2),
     }
@@ -66,6 +66,7 @@ impl PieceKind {
 const INITIAL_X: usize = 2;
 const BOARD_WIDTH: usize = 7;
 
+#[derive(Debug)]
 struct Piece {
   /// width, height
   size: (usize, usize),
@@ -114,7 +115,7 @@ impl Board {
   }
 
   /// If the piece can be blown in the given direction, do so.
-  fn blow(&self, piece: &mut Piece, wind: &Wind) {
+  fn blow(&self, piece: &mut Piece, wind: Wind) {
     if let Some(new_x) = wind.blow(piece.x, BOARD_WIDTH + 1 - piece.size.0) {
       if piece.not_blocked(new_x, &self.filled[piece.y..piece.y + piece.size.1]) {
         piece.x = new_x;
@@ -137,6 +138,7 @@ impl Board {
   }
 }
 
+/*
 impl Display for Board {
   fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
      for row in (0..self.current_height).rev() {
@@ -148,21 +150,95 @@ impl Display for Board {
     write!(f, "\n")
   }
 }
+*/
 
-const NUM_ROCKS: usize = 2022;
+struct WindIter<'a> {
+  data: &'a[Wind],
+  posn: usize,
+}
 
-pub fn part1(input: &InputType) -> OutputType {
-  let mut board = Board::new(NUM_ROCKS * 3);
-  let mut wind_itr = input.iter().cycle();
-  let mut pieces: Vec<Piece> = [PieceKind::Bar, PieceKind::Plus, PieceKind::El,
+impl<'a> WindIter<'a> {
+  fn new(data: &'a[Wind]) -> Self {
+    WindIter{data, posn: 0}
+  }
+
+  fn next(&mut self) -> Wind {
+    let old = self.posn;
+    self.posn = (self.posn + 1) % self.data.len();
+    self.data[old]
+  }
+
+  fn get_posn(&self) -> usize {
+    self.posn
+  }
+}
+
+#[derive(Debug)]
+struct CycleInformation {
+  kind: usize,
+  wind: usize,
+  start: usize,
+  height: usize,
+}
+
+#[derive(Debug)]
+struct TailInformation {
+  remaining_pieces: usize,
+  extra_rows: usize,
+}
+
+pub fn drop_rocks(input: &InputType, count: usize) -> OutputType {
+  let mut seen: HashSet<(usize,usize)> = HashSet::new();
+  let mut cycle: Option<CycleInformation> = None;
+  let mut tail: Option<TailInformation> = None;
+  let mut board = Board::new(500_000);
+  let mut wind_itr = WindIter::new(input);
+  let mut pieces: Vec<Piece> = [PieceKind::Bar, PieceKind::Plus, PieceKind::L,
     PieceKind::I, PieceKind::Square].iter().map(|k| Piece::new(k.clone())).collect();
   let num_pieces = pieces.len();
-  for piece_count in 0..NUM_ROCKS {
+  for piece_count in 0..count {
+    // Check for cycles in the piece kind and wind
+    let kind_and_wind = (piece_count % num_pieces, wind_itr.get_posn());
+    // Are we computing the tail?
+    if let Some(info) = &mut tail {
+      // just count down the remaining rows
+      if info.remaining_pieces == 0 {
+        break;
+      } else {
+        info.remaining_pieces -= 1;
+      }
+    } else if let Some(info) = &cycle {
+      // Check to see if we have found the second iteration of the cycle. If so,
+      // compute the length of the cycle and how high our virtual stack needs to be.
+      if (info.kind, info.wind) == kind_and_wind {
+        let cycle_time = piece_count - info.start;
+        let cycle_height = board.current_height - info.height;
+        let remaining = count - piece_count;
+        // Set the tail information so that we handle the last partial cycle
+        tail = Some(TailInformation{remaining_pieces: (remaining - 1) % cycle_time,
+          extra_rows: (remaining / cycle_time) * cycle_height});
+        if remaining % cycle_time == 0 {
+          break
+        }
+      }
+    } else {
+      // Look for a repeat of kind and wind to identify the cycle.
+      // The first cycle is usually different, but the second and later cycles are
+      // identical. After the first, we set the cycle information and use that to
+      // identify the second iteration.
+      if seen.contains(&kind_and_wind) {
+        seen.clear();
+        cycle = Some(CycleInformation{kind: kind_and_wind.0, wind: kind_and_wind.1,
+          start: piece_count, height: board.current_height});
+      } else {
+        seen.insert(kind_and_wind);
+      }
+    }
     let piece = &mut pieces[piece_count % num_pieces];
     // simulate the initial 3 level fall
     piece.reset(board.current_height);
     for _ in 0..=NEW_PIECE_HEIGHT {
-      let wind = wind_itr.next().unwrap();
+      let wind = wind_itr.next();
       if let Some(new_x) = wind.blow(piece.x, BOARD_WIDTH + 1 - piece.size.0) {
         piece.x = new_x;
       }
@@ -170,21 +246,27 @@ pub fn part1(input: &InputType) -> OutputType {
     // now check
     while board.can_fall(piece) {
       piece.y -= 1;
-      board.blow(piece, wind_itr.next().unwrap());
+      board.blow(piece, wind_itr.next());
     }
     board.place_piece(piece);
+  }
+  if let Some(info) = tail {
+    board.current_height += info.extra_rows;
   }
   board.current_height
 }
 
-pub fn part2(_: &InputType) -> OutputType {
-  0
+pub fn part1(input: &InputType) -> OutputType {
+  drop_rocks(input, 2_022)
+}
+
+pub fn part2(input: &InputType) -> OutputType {
+  drop_rocks(input, 1_000_000_000_000)
 }
 
 #[cfg(test)]
 mod tests {
   use crate::day17::{generator, part1, part2};
-
 
   #[test]
   fn test_part1() {
@@ -195,7 +277,7 @@ mod tests {
 
   #[test]
   fn test_part2() {
-    //assert_eq!(93, part2(&generator(INPUT)));
+    assert_eq!(1514285714288, part2(&generator(INPUT)));
   }
 
   const INPUT: &str = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>\n";
