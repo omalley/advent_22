43e4b4119b81898185dd5401116c4ee4c1b0d88f
Owen O'Malley <omalley@apache.org>
2023-08-03 14:05:08 -0700

    remove function and one field

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,17 @@ use std::time::Instant;
 #[derive(Clone,Copy,Debug,Eq,Hash,PartialEq)]
 struct Position {
   x: i32,
-  y: i32,
-}
-
-fn everybody_move(x: i32) -> usize {
-  let mut map: HashMap<Position,usize> = HashMap::with_capacity(10_000);
-  for y in 0..10_000 {
-    map.insert(Position{x, y}, 0);
-  }
-  map.len()
 }
 
 fn main() {
   let mut result = 0;
   let start = Instant::now();
   for x in 0..10_000 {
-    result += everybody_move(x);
+    let mut map: HashMap<Position,usize> = HashMap::with_capacity(10_000);
+    for y in 0..10_000 {
+      map.insert(Position{x: x * 10_000 + y}, 0);
+    }
+    result += map.len();
   }
   println!("Time elapsed is: {:?} = {result}", start.elapsed());
 }
